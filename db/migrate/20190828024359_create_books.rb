51d9fcee3b80bfa484b34f493dbf6f1c907ab975
tresagua0123 <gentlekai10@gmail.com>
2019-08-28 13:49:08 +0900

    improved new and edit pages

--- db/migrate/20190828024359_create_books.rb
+++ db/migrate/20190828024359_create_books.rb
@@ -1,10 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :name
-      t.string :author
-      t.text :description
-      t.integer :price
+      t.string :name, null: false
+      t.string :author, null: false
+      t.text :description, null: false
+      t.integer :price, null: false
       t.boolean :availability, default: true
 
       t.timestamps
