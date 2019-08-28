74e5712196c4eed492d510035d6884a3e820da3f
tresagua0123 <gentlekai10@gmail.com>
2019-08-28 16:14:11 +0900

    added user book association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_024359) do
+ActiveRecord::Schema.define(version: 2019_08_28_070347) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_08_28_024359) do
     t.boolean "availability", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
